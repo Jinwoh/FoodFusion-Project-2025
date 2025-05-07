83591a6597531093449675e97e3a7c39598a8e99
Alex Parra <alex.parra@gonzalezgimenez.com>
2025-05-07 16:06:54 -0300

    Modificaciones en el admin

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'restaurante.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': config('DB_ENGINE'),
-        'NAME': config('DB_NAME'),
+        'NAME': 'pruebas',
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
